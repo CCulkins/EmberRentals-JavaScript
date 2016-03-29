c3aacf0948eb4e1095cb2af60c55d0fd5c615c6c
Cassandra Culkins and Mao Thygesen <cassy.culkins@gmail.com>
2016-03-29 09:28:58 -0700

    add update rental by finind record in firebase

---
+++ app/routes/index.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      announcements: this.store.findAll('announcement'),
+      rentals: this.store.findAll('rental')
+    });
+  },
+  actions: {
+    save3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
+    keep3(params) {
+      var newAnnouncement = this.store.createRecord('announcement', params);
+      newAnnouncement.save();
+      this.transitionTo('index');
+    },
+    destroyAnnouncement(announcement){
+      announcement.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+});
